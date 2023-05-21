69391dd84cf07617e57c982f8831a0e33219adaa
Emily McLean <emilymclean93@gmail.com>
2023-05-20 20:39:35 -0400

    pseudo coded user and thought routes. set up index.js in api directory

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+//? Get all thoughts
+
+//? Get a single thought by ID
+
+//? Post to create new thought (push created thought's id to associated user's thoughts array field)
+
+//? Put to update a thought by id
+
+//? Delete to remove a thought by id
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+
+//* for the api/users
+
+//? Get all users
+
+//? Get single user by id and the thought and friend data
+
+//? Post a new user
+
+//? Put to update a user by ID
+
+//? Delete to remove a user by ID
+    //? BONUS remove the user's associated thoughts when deleted
+
+
+//* for the /api/users/:userId/friends/:friendId
+
+//? post to add a new friend to user's friend list
+//? delete to remove friend from a user's friend list
