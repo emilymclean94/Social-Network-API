d84efca92b425626fa3430eec376cd74221436fe
Emily McLean <emilymclean93@gmail.com>
2023-05-21 21:46:28 -0400

    added functions to thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,14 +1,25 @@
 const router = require('express').Router();
 
 //! All associated functions listed in the curly brackets
-const { } = require('../../controllers/thoughtController');
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    deleteThought,
+    updateThought,
+ } = require('../../controllers/thoughtController');
 
 //? Get all thoughts
 //? Post to create new thought (push created thought's id to associated user's thoughts array field)
-
+router.route('/').get(getThought).post(createThought);
 
 //? Get a single thought by ID
 //? Put to update a thought by id
 //? Delete to remove a thought by id
+router.route('/:thoughtId')
+.get(getSingleThought)
+.delete(deleteThought)
+.put(updateThought);
+
 
 module.exports = router;
