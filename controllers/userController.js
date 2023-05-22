59928697ab3145c4783955e24a2ed209c6d00be9
Emily McLean <emilymclean93@gmail.com>
2023-05-21 21:24:49 -0400

    add and remove friend functions coded

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,7 +44,100 @@ async createUser(req, res) {
   }
 },
 
-// Delete student deleteUser()
+  // Update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(course);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+// Delete user deleteUser()
     // BONUS cascade delete thoughts associated with user
+    async deleteUser(req, res) {
+      try {
+        const user = await User.findOneAndRemove({ _id: req.params.userId });
+  
+        if (!user) {
+          return res.status(404).json({ message: 'No such user exists' });
+        }
+  
+    //! This is where we delete the posts but not sure how yet
+        // const reaction = await Thought.findOneAndUpdate(
+        //   { users: req.params.userId },
+        //   { $delete: { users: req.params.userId } },
+        //   { new: true }
+        // );
+  
+        // if (!course) {
+        //   return res.status(404).json({
+        //     message: 'User deleted, but no courses found',
+        //   });
+        // }
+  
+        res.json({ message: 'User successfully deleted' });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    },
+
+//! Copied from mini project
+      // Add an assignment to a user
+  async addFriend(req, res) {
+    console.log('You are adding a friend');
+    console.log(req.body);
+
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friend: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID :(' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //!Is this how we also delete posts?
+  // Remove friend from a user
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friend: { friendId: req.params.friendId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID :(' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
 };
