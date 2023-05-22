55c8b01cf36e946bc52df69a6c20e0a70e8848a5
Emily McLean <emilymclean93@gmail.com>
2023-05-21 21:42:35 -0400

    update thought function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,60 +1,79 @@
 const { ObjectId } = require('mongoose').Types;
-const { User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
 
-// Get all thoughts
-async getThought(req, res) {
-    try {
-      const thought = await Thought.find();
-      res.json(thought);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+    // Get all thoughts
+    async getThought(req, res) {
+        try {
+            const thought = await Thought.find();
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 
-  // Get a single thought
-  async getSingleThought(req, res) {
-    try {
-      const thought = await Thought.findOne({ _id: req.params.thoughtId })
-        .select('-__v');
+    // Get a single thought
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
+                .select('-__v');
 
-      if (!thought) {
-        return res.status(404).json({ message: 'No thought with that ID' });
-      }
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
 
-      res.json(thought);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 
-  // Create thought
-  //! Need to push this to user's thoughts array field somehow
-  async createThought(req, res) {
-    try {
-      const thought = await Thought.create(req.body);
-      res.json(thought);
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
-  },
+    // Create thought
+    //! Need to push this to user's thoughts array field somehow
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
-  // Delete thought
+    // Delete thought
     async deleteThought(req, res) {
         try {
-          const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
-    
-          if (!thought) {
-            res.status(404).json({ message: 'No thought with that ID' });
-          }
-    
-          await Student.deleteMany({ _id: { $in: thought.students } });
-          res.json({ message: 'thought and students deleted!' });
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with that ID' });
+            }
+
+            await Student.deleteMany({ _id: { $in: thought.students } });
+            res.json({ message: 'thought and students deleted!' });
         } catch (err) {
-          res.status(500).json(err);
+            res.status(500).json(err);
         }
-      },
+    },
+
+    // Update thought
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 
 };
